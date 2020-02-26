22657c66999385b366ab6ce07a4cca32c9abcacb
hyecheon <rainbow880616@gmail.com>
2020-02-26 23:13:59 +0900

    클릭 이벤트.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"github.com/hajimehoshi/ebiten/inpututil"
 	"log"
 )
 
@@ -33,9 +34,13 @@ const (
 )
 
 var (
-	board     [BoardWidth][BoardHeight]GimulType
-	bgimg     *ebiten.Image
-	gimulImgs [GimulTypeMax]*ebiten.Image
+	board       [BoardWidth][BoardHeight]GimulType
+	bgimg       *ebiten.Image
+	selectedImg *ebiten.Image
+	gimulImgs   [GimulTypeMax]*ebiten.Image
+	selected    bool
+	selectedX   int
+	selectedY   int
 )
 
 func main() {
@@ -101,10 +106,31 @@ func loadImages(err error) error {
 	if err != nil {
 		log.Fatalf("read file error: %v", err)
 	}
+	selectedImg, _, err = ebitenutil.NewImageFromFile("./images/selected.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatalf("read file error: %v", err)
+	}
+
 	return err
 }
 
 func update(screen *ebiten.Image) error {
+	if inpututil.IsMouseButtonJustReleased(ebiten.MouseButtonLeft) {
+		x, y := ebiten.CursorPosition()
+		i, j := x/GridWidth, y/GridHeight
+		if selected {
+			if i == selectedX && j == selectedY {
+				selected = false
+			} else {
+				move(selectedX, selectedY, i, j)
+			}
+		} else {
+			if board[i][j] != GimulTypeNone {
+				selected = true
+				selectedX, selectedY = i, j
+			}
+		}
+	}
 	screen.DrawImage(bgimg, nil)
 	for i := 0; i < BoardWidth; i++ {
 		for j := 0; j < BoardHeight; j++ {
@@ -138,5 +164,44 @@ func update(screen *ebiten.Image) error {
 			}
 		}
 	}
+	if selected {
+		opts := &ebiten.DrawImageOptions{}
+		opts.GeoM.Translate(float64(GimulStartX+GridWidth*selectedX), float64(GimulStartY+GridHeight*selectedY))
+		screen.DrawImage(selectedImg, opts)
+	}
 	return nil
+}
+
+func move(prevX int, prevY int, tarX int, tarY int) {
+	if isMovable(prevX, prevY, tarX, tarY) {
+		board[prevX][prevY], board[tarX][tarY] = GimulTypeNone, board[prevX][prevY]
+		selected = false
+	}
+}
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+func isMovable(prevX int, prevY int, tarX int, tarY int) bool {
+	if tarX < 0 || tarY < 0 {
+		return false
+	}
+	if tarX >= BoardWidth || tarY >= BoardHeight {
+		return false
+	}
+	switch board[prevX][prevY] {
+	case GimulTypeGreenJa:
+		return prevX+1 == tarX && prevY == tarY
+	case GimulTypeRedJa:
+		return prevX-1 == tarX && prevY == tarY
+	case GimulTypeGreenSang, GimulTypeRedSang:
+		return (abs(prevX-tarX) == 1) && (abs(prevY-tarY) == 1)
+	case GimulTypeGreenJang, GimulTypeRedJang:
+		return abs(prevX-tarX)+abs(prevY-tarY) == 1
+	case GimulTypeGreenWang, GimulTypeRedWang:
+		return (abs(prevX-tarX) == 1) || (abs(prevY-tarY) == 1)
+	}
+	return false
 }
